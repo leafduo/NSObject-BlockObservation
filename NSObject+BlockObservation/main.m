4e1cff6c76df76c6bc117dcd96e42dc18b92834f
Zuyang Kou <leafduo@gmail.com>
2013-02-21 17:04:52 +0800

    Add a new project.
    Seems I have to do that to make the program run

---
+++ NSObject+BlockObservation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSObject+BlockObservation
+//
+//  Created by leafduo on 2/21/13.
+//  Copyright (c) 2013 leafduo.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
